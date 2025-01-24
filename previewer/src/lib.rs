4f8041fc75c37d9bd9aab527998a3ce560dabec2
Kenny Null <timteillor@gmail.com>
2025-01-24 17:43:32 +0000

    Create previewer lib

---
+++ previewer/src/lib.rs
@@ -0,0 +1,46 @@
+use image::{imageops::FilterType, DynamicImage};
+
+pub fn preview(image: DynamicImage, preview_size: Size, filter: FilterType) -> Result<DynamicImage, Error> {
+    if preview_size.width == 0 {
+        return Err(Error::InvalidWidth);
+    }
+
+    if preview_size.height == 0 {
+        return Err(Error::InvalidHeight);
+    }
+
+    let image = image.resize(preview_size.width, preview_size.height, filter);
+
+    Ok(image)
+}
+
+pub struct Size {
+    pub width: u32,
+    pub height: u32,
+}
+
+pub enum Error {
+    InvalidHeight,
+    InvalidWidth,
+}
+
+pub fn filter_to_string(filter: &FilterType) -> String {
+    match filter {
+        FilterType::Nearest => "nearest".into(),
+        FilterType::Triangle => "triangle".into(),
+        FilterType::CatmullRom => "cubic".into(),
+        FilterType::Gaussian => "gauss".into(),
+        FilterType::Lanczos3 => "lanczos3".into(),
+    }
+}
+
+pub fn str_to_filter(filter: &str) -> Option<FilterType> {
+    match filter {
+        "nearest" => Some(FilterType::Nearest),
+        "triangle" => Some(FilterType::Triangle),
+        "cubic" => Some(FilterType::CatmullRom),
+        "gauss" => Some(FilterType::Gaussian),
+        "lanczos3" => Some(FilterType::Lanczos3),
+        _ => None,
+    }
+}
